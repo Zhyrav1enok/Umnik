f47258362eb821aa7198ef19a7a588d23e50bd12
Revanh13 <vana130102@icloud.com>
2021-01-07 22:52:08 +0500

    Add pointer

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject canvas;
+    public GameObject bot;
+
+    private Animator botAnimator;
+    
+    void Start()
+    {
+        canvas.SetActive(false);
+        botAnimator = bot.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void BotButtonAwake()
+    {
+        StartCoroutine(BotAwake());
+    }
+
+    public void BotButtonSleep()
+    {
+        StartCoroutine(BotSleep());
+    }
+
+    private IEnumerator BotAwake()
+    {
+        botAnimator.SetTrigger("Awake");
+        yield return new WaitForSeconds(1.5f);   // Ждем время анимации бота
+        canvas.SetActive(true);
+    }
+    
+    private IEnumerator BotSleep()
+    {
+        botAnimator.SetTrigger("Sleep");
+        yield return new WaitForSeconds(1.5f);   // Ждем время анимации бота
+        canvas.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/VRInputModule.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class VRInputModule : BaseInputModule
+{
+    [SerializeField] private Pointer pointer;
+
+    public PointerEventData Data { get; private set; } = null;
+
+    //public AudioManager audioManager;
+
+    protected override void Start()
+    {
+        Data = new PointerEventData(eventSystem);
+        Data.position = new Vector2(pointer.Camera.pixelWidth / 2, pointer.Camera.pixelHeight / 2);
+    }
+
+    public override void Process()
+    {
+        eventSystem.RaycastAll(Data, m_RaycastResultCache);
+        Data.pointerCurrentRaycast = FindFirstRaycast(m_RaycastResultCache);
+
+        HandlePointerExitAndEnter(Data, Data.pointerCurrentRaycast.gameObject);
+    }
+
+    public void Press()
+    {
+        Data.pointerPressRaycast = Data.pointerCurrentRaycast;
+
+        Data.pointerPress = ExecuteEvents.GetEventHandler<IPointerClickHandler>(Data.pointerPressRaycast.gameObject);
+
+        ExecuteEvents.Execute(Data.pointerPress, Data, ExecuteEvents.pointerDownHandler);
+        
+        //audioManager.Play("UISelection");
+    }
+
+    public void Release()
+    {
+        GameObject pointerRelease = ExecuteEvents.GetEventHandler<IPointerClickHandler>(Data.pointerCurrentRaycast.gameObject);
+
+        if (Data.pointerPress == pointerRelease)
+            ExecuteEvents.Execute(Data.pointerPress, Data, ExecuteEvents.pointerClickHandler);
+
+        ExecuteEvents.Execute(Data.pointerPress, Data, ExecuteEvents.pointerUpHandler);
+
+        Data.pointerPress = null;
+
+        Data.pointerCurrentRaycast.Clear();
+    }
+}
