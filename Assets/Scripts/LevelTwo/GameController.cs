671af238a138415c86d0db3a39eb6ee492e768ad
Ivan <ivan@kakashi-hatake.ru>
2021-04-04 22:37:40 +0500

    start level two

---
+++ Assets/Scripts/LevelTwo/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LevelTwo
+{
+    public class GameController : MonoBehaviour
+    {
+        void Start()
+        {
+        
+        }
+
+        void Update()
+        {
+        
+        }
+    }
+}
+
